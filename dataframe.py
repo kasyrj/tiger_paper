d28ad277464a841558baa1b57bd9147292637f09
Luke Maurits <luke@maurits.id.au>
2019-05-28 14:25:15 +0300

    Replace old harvest variable names with more transparent alternatives.

--- dataframe.py
+++ dataframe.py
@@ -10,30 +10,31 @@ class DataFrame:
 
     def format_output(self):
         """Return a string containing a .csv file of the data."""
-        iso_codes = list(self.data.keys())
-        iso_codes.sort()
-        fnames = list(self.data[iso_codes[0]].keys())
-        fnames.sort()
+        languages = list(self.data.keys())
+        languages.sort()
+        features = list(self.data[languages[0]].keys())
+        features.sort()
         lines = []
-        lines.append("iso,"+",".join(fnames))
-        for iso in iso_codes:
-            lines.append(iso + "," + ",".join(map(str,[self.data[iso][f] for f in fnames])))
+        lines.append("language,"+",".join(features))
+        for l in languages:
+            lines.append(l + "," + ",".join(map(str,[self.data[l][f] for f in features])))
         return "\n".join(lines)
 
     def borrow(self, borrowing_rate):
         """Randomly borrow feature values at a certain rate."""
         if borrowing_rate == 0.0:
             return
-        iso_codes = list(self.data.keys())
-        fnames = list(self.data[iso_codes[0]].keys())
-        for f in fnames:
+        languages = list(self.data.keys())
+        languages.sort()
+        features = list(self.data[languages[0]].keys())
+        for f in features:
             borrowable_values = []
             borrowers = []
-            for iso in iso_codes:
+            for l in languages:
                 if random.random() <= borrowing_rate:
-                    borrowers.append(iso)
-            for iso in borrowers:
-                current_value = self.data[iso][f]
-                borrowable_values = [self.data[i][f] for i in iso_codes if i!=iso]
+                    borrowers.append(l)
+            for l in borrowers:
+                current_value = self.data[l][f]
+                borrowable_values = [self.data[l][f] for L in languages if L!=l]
                 assert current_value not in borrowable_values
-                self.data[iso][f] = random.sample(borrowable_values,1)[0]
+                self.data[l][f] = random.sample(borrowable_values,1)[0]
